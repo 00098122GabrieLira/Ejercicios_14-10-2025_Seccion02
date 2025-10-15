71cf505102c43e9f27ce66d10943f0a5b77e9378
IgnacioPeralta00 <00196321@uca.edu.sv>
2025-10-14 18:14:55 -0600

    feat: agregar módulos para leer archivos JSON del sistema

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
